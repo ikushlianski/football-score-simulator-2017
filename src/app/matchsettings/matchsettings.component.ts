9efc84b60026c3a6648c0d9a7f4c7bd08207a189
Ilya Kushlianski <kushliansky@gmail.com>
2017-06-30 13:46:55 +0300

    add home and away team name binding and second leg binding

--- src/app/matchsettings/matchsettings.component.ts
+++ src/app/matchsettings/matchsettings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-matchsettings',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matchsettings.component.css']
 })
 export class MatchsettingsComponent implements OnInit {
+  // team names
+  homeTeamName = undefined;
+  awayTeamName = undefined;
+  // second leg stuff
+  isSecondLeg:boolean = false;
+  awayTeam1stLegGoals = undefined;
+  homeTeam1stLegGoals = undefined;
+  onChangeSecondLeg(event){
+    console.log(this.homeTeamName, this.awayTeamName)
+    if (event.target.checked){
+      this.isSecondLeg = true;
+    } else {
+      this.isSecondLeg = false;
+      this.awayTeam1stLegGoals = undefined;
+      this.homeTeam1stLegGoals = undefined;
+    };
+  }
+  onChangeAwayTeam1stLegGoals(event) {
+    this.awayTeam1stLegGoals = event.target.value;
+  }
+  onChangeHomeTeam1stLegGoals(event) {
+    this.homeTeam1stLegGoals = event.target.value;
+  }
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    $('.firstLegGoalsSetter').hide();
+    $("#isSecondLeg").change(function(event){
+      if (event.target.checked) {
+        $('.firstLegGoalsSetter').show();
+      } else {
+        $('.firstLegGoalsSetter').hide();
+      }
+    });
   }
 
 }
